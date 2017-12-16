d8e567a0681352827661d975d5ade7a5606eb32a
Rex Tsou <akccakccwww@gmail.com>
2017-12-16 22:53:07 +0800

    Fix post title because it doesn't compile properly

--- clone.sh
+++ clone.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-# clone source branch and submodules(themes)
-git clone --recursive https://github.com/akccakcctw/akccakcctw.github.io.git --branch source
+# clone source branch and submodules (themes)
+git clone --recursive https://github.com/akccakcctw/blog.git --branch source
 
-cd akccakcctw.github.io
+cd blog
 
-# clone master branch
-git clone https://github.com/akccakcctw/akccakcctw.github.io.git --branch master public/
+# clone master branch to public/
+git clone https://github.com/akccakcctw/blog.git --branch master public/
 
 # create folders if not exist
-mkdir -p archetypes data layouts static
+for folder in {archetypes,data,layouts,static}; do
+  [ ! -d "$folder" ] && mkdir "$folder"
+done
+unset folder
 
