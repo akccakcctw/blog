466f6ef8df70fbf26d0501c2b3ecd7b46ab58605
Rex Tsou <rex.tsou@kkday.com>
2019-03-01 15:31:32 +0800

    Update new-post.sh

--- tools/new-post.sh
+++ tools/new-post.sh
@@ -5,10 +5,11 @@ BASE_DIR="$(dirname $0)"
 source "${BASE_DIR}"/helpers.sh
 
 read -p 'Post type: [post] ' post_type
-read -p 'Post title: (ex: 2018-12-01-hello-world)' post_title
+read -p 'Post title: (ex: 2018-12-01-hello-world) ' post_title
 post_type="${post_type:-post}"
 
 if [[ -z "${post_type}" || -z "${post_title}" ]]; then
+  exit "Please set `post_type` and `post_title`."
   exit 1
 fi
 
